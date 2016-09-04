f3cf4d9bb0b9399d206e409d48d82c2c41704722
Bach Le <bach@bullno1.com>
2016-09-04 12:46:31 +0800

    Add example for on_reload attribute

--- src/example.erl
+++ src/example.erl
@@ -1,5 +1,9 @@
 -module(example).
--export([test/0]).
+-export([test/0, reload/0]).
+-on_reload(reload/0).
 
 % Change this function to return something else
 test() -> 1.
+
+reload() ->
+	io:format("Module reloaded~n").
